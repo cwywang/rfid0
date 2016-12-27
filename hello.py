8f2ff6b8fa18c4cd91a942df7d4d16455a28d51f
cwywang <741077081@qq.com>
2016-12-27 13:57:34 +0800

    5

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 import re
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import request,jsonify
 basedir = os.path.abspath(os.path.dirname(__file__))
 #from send_mail import Send_Mail
 App=Flask(__name__)
@@ -10,8 +11,8 @@ App.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 App.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db=SQLAlchemy(App)
 #mail=Send_Mail('system@email.doforyou.gift','Aa741077081')
-@App.route('/rfidcard',methods=['POST'])
-def rfidcard():
+@App.route('/postCard',methods=['POST'])
+def postCard():
 	card_id=request.form['card_id']
 	user_id=request.form['user_id']
 	card_time=request.form['card_time']
@@ -23,8 +24,18 @@ def rfidcard():
 	db.session.add(rfidcard)
 	db.session.commit()
 	return 'post success',200
-@App.route('/card1',methods=['GET'])
-def card1():
+@App.route('/getCard/<card_id>',methods=['GET'])
+def getCard(card_id):
+	card=RfidCard.query.filter_by(card_id=card_id).first()
+	if card:
+		return jsonify({'card_id':card.card_id,
+						'user_id':card.user_id,
+						'card_time':card.card_time,
+						'freeze':card.freeze})
+	else:
+		return 'not found',404
+@App.route('/addCard',methods=['GET'])
+def addCard():
 	rfidcard=RfidCard(card_id="card_id",
 						user_id="user_id",
 						card_time="card_time",
@@ -32,10 +43,6 @@ def card1():
 	db.session.add(rfidcard)
 	db.session.commit()
 	return "123",200
-@App.route('/card',methods=['GET'])
-def card():
-	Rcard=RfidCard.query.filter_by(card_id="card_id").first()
-	return Rcard.card_id+"123",200
 @App.route('/init',methods=['GET'])
 def init():
 	db.drop_all()
